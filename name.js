8384c8a3a39bb2a638802c5160d3e0e13c9b0f19
egorburahek <burachekgogo@gmail.com>
2025-05-10 21:08:37 +0300

    comment

--- name.js
+++ name.js
@@ -1,10 +1,35 @@
 document.getElementById("start").addEventListener("click",function(){
     document.getElementById(question1)}) /*Денамічни перехід між секціями*/
+
 document.getElementById("Correct1").addEventListener("click",function(){
-    document.getElementById(question2)})    
+    document.getElementById(question2)})
+
 document.getElementById("Correct2").addEventListener("click",function(){
-    document.getElementById(question3)})  
+    document.getElementById(question3)}) 
+
     document.getElementById("Correct3").addEventListener("click",function(){
-    document.getElementById(question4)})  
-    document.getElementById("Wrong").addEventListener("click",function(){
-    document.getElementById(beginning)}) 
+    document.getElementById(question4)})
+
+    document.getElementById("Correct4").addEventListener("click",function(){
+    document.getElementById(question5)})
+  
+     document.getElementById("Correct5").addEventListener("click",function(){
+    document.getElementById(question6)})
+
+     document.getElementById("Correct6").addEventListener("click",function(){
+    document.getElementById(question7)})
+
+    document.getElementById("Correct7").addEventListener("click",function(){
+    document.getElementById(question8)})
+
+    document.getElementById("Correct8").addEventListener("click",function(){
+    document.getElementById(question9)})
+
+      document.getElementById("Correct9").addEventListener("click",function(){
+    document.getElementById(finish)})
+
+      document.getElementById("End").addEventListener("click",function(){
+    document.getElementById(beginning)})  
+
+     document.getElementById("Wrong").addEventListener("click",function(){
+    document.getElementById(beginning)})  
