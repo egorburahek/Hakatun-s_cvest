60d6571b478a83a80df20d9c8beefc23342ae1e2
egorburahek <burachekgogo@gmail.com>
2025-05-10 16:23:12 +0300

    comment

---
+++ name.js
@@ -0,0 +1,10 @@
+document.getElementById("start").addEventListener("click",function(){
+    document.getElementById(question1)}) /*Денамічни перехід між секціями*/
+document.getElementById("Correct1").addEventListener("click",function(){
+    document.getElementById(question2)})    
+document.getElementById("Correct2").addEventListener("click",function(){
+    document.getElementById(question3)})  
+    document.getElementById("Correct3").addEventListener("click",function(){
+    document.getElementById(question4)})  
+    document.getElementById("Wrong").addEventListener("click",function(){
+    document.getElementById(beginning)}) 
